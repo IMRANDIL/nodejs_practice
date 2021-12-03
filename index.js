6dc9aba4f81986dcb1d2d6ffa3800b6cff0a6dcb
IMRANDIL <aliimranadil2@gmail.com>
2021-12-03 17:58:49 +0530

    yh

--- index.js
+++ index.js
@@ -9,19 +9,39 @@ const http = require('http');
 
 // creating server object...
 
-const server = http.createServer((req, res)=>{
-    res.setHeader('content-type', 'TEXT/HTML');
-    res.write('<html>');
-    res.write('<head><title>my first node js application without any framework!</title></head>');
-    res.write('<body><h2>Hello People from node js server! </h2></body>');
-    res.write('</html>');
-res.end();
-    
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+
+        // res.setHeader('content-type', 'TEXT/HTML');
+        res.write('<html>');
+        res.write('<head><title>my first node js application without any framework!</title></head>');
+        res.write('<body><h2>Hello People from node js server! </h2></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/about') {
+
+        // res.setHeader('content-type', 'TEXT/HTML');
+        res.write('<html>');
+        res.write('<head><title>my first node js application without any framework!</title></head>');
+        res.write('<body><h2>Hello People, I am a Fulll Stack Developer! </h2></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
 });
 
 
 //server setup..
 
-server.listen(3000, ()=>{
-    console.log('server is running on port 3000');
+server.listen(4000, () => {
+    console.log('server is running on port 4000');
 })
+
+
+
+
+
+
